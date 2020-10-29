c630fa78e0b64514ec3af50f60a6489986bdaac2
xujia <2315498692@qq.com>
2020-10-29 16:38:07 +0800

    登录界面

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <!--
  * @Author: your name
  * @Date: 2020-10-20 10:23:15
- * @LastEditTime: 2020-10-21 10:45:02
- * @LastEditors: your name
+ * @LastEditTime: 2020-10-28 13:46:47
+ * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \vue.test\src\App.vue
 -->
@@ -24,7 +24,7 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
   margin-top: 60px;
 }
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,25 +1,88 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-10-21 10:21:14
+ * @LastEditTime: 2020-10-29 13:43:58
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \vue.test\src\components\Login.vue
+-->
 <!--  -->
 <template>
-  <div class="">哈哈哈哈哈</div>
+  <div>
+    <!-- <div>{{ message }}</div>
+    <div v-for="(item, i) in arr" :key="i">
+      {{ item }}
+    </div>
+    <ul>
+      <li v-for="(item, i) in arrObj" :key="i">{{ item.name + item.age }}</li>
+    </ul>
+    <h1>当前计数：{{ num }}</h1>
+    <button @click="add()">+</button>
+    <button @click="reduce()">-</button> -->
+    <div class="forms">
+      <el-form ref="form" :model="form" label-width="80px">
+        <el-form-item label="用户名">
+          <el-input v-model="form.name"></el-input>
+        </el-form-item>
+        <el-form-item label="密码">
+          <el-input v-model="form.pwd"></el-input>
+        </el-form-item>
+        <el-form-item style="text-align: center">
+          <el-button type="primary" @click="onSubmit">登陆</el-button>
+          <el-button>注册</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
 </template>
 
 <script>
 //这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
 //例如：import 《组件名称》 from '《组件路径》';
-
+import { login } from "@/request/api"; // 导入我们的api接口
 export default {
   //import引入的组件需要注入到对象中才能使用
   components: {},
   data() {
     //这里存放数据
-    return {};
+    return {
+      message: "哈哈哈哈哈1",
+      arr: ["a", "b", "c"],
+      arrObj: [
+        { name: "aaaa", age: 19 },
+        { name: "ccccc", age: 20 },
+      ],
+      num: 0,
+      form: {
+        name: "",
+        pwd: "",
+      },
+    };
   },
   //监听属性 类似于data概念
   computed: {},
   //监控data中的数据变化
   watch: {},
   //方法集合
-  methods: {},
+  methods: {
+    add() {
+      this.num++;
+    },
+    reduce() {
+      this.num--;
+    },
+    onSubmit() {
+      const { name, pwd } = this.form;
+      console.log("name!", name);
+      console.log("pwd!", pwd);
+      login({
+        userName: name,
+        userPwd: pwd,
+      }).then((res) => {
+        console.log(res);
+      });
+    },
+  },
   //生命周期 - 创建完成（可以访问当前this实例）
   created() {},
   //生命周期 - 挂载完成（可以访问DOM元素）
@@ -34,4 +97,8 @@ export default {
 };
 </script>
 <style scoped>
+.forms {
+  margin: 0 auto;
+  width: 500px;
+}
 </style>
